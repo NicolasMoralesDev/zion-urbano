abc8e8037461081525c406e04d2bde90a545aca3
Nicolas <nicolasmoralesj03@gmail.com>
2022-10-22 20:47:33 -0300

    se agrega link a whatsapp en los botones del catalogo

--- components/Productos.js
+++ components/Productos.js
@@ -1,21 +1,18 @@
 import React, { useState }  from 'react'
 import Card from 'react-bootstrap/Card';
-import Head from 'next/head'
 
 const Productos = (props) => {
   
   const articulos = props.card.filter(datos => datos.categoria.nombre == props.titulo);
-  const consulta = ()=>{
-   window.location='http://es.stackoverflow.com';
+
+  const consulta = (producto)=>{
+   window.location=`https://api.whatsapp.com/send?phone=541165759558&text=!Hola%C2%A1%20%F0%9F%98%80%20Acabo%20de%20visitar%20tu%20catalogo%20web%20y%20quer%C3%ADa%20consultarte%20sobre%20este%20articulo%20${producto}%20%F0%9F%98%84`;
   };
+
   const  [productos, setProductos] = useState(articulos);
 
   return(
     <>
-    <Head>
-    <script src="https://unpkg.com/scrollreveal"></script>
-        <script src='../pages/js/scrollReveal.js'></script>
-    </Head>
   <div className='row  catalogo_container'>
     <h5 className='text-left titulo-categoria'>{props.titulo}:</h5>
      { productos.map(element => {
@@ -31,7 +28,7 @@ const Productos = (props) => {
               </Card.Text>
               <h3 className='precio-card'>${element.precio}</h3>
 
-              <button className="custom-btn btn-12"  role="link" onClick={()=> consulta()}><span>Click!!</span><span>Pregunta!</span></button>
+              <button className="custom-btn btn-12"  role="link" onClick={()=> consulta(element.nombre)}><span>Click!!</span><span>Pregunta!</span></button>
             </Card.Body>
           </Card>
           )     
